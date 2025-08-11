54c26b7d2d57e368a112afa58d750ca5c286d7b2
Gene Graves <gemdevelopers@myokapis.net>
2025-08-11 07:36:52 -0500

    Added more code comments to the service projects.

--- TsysService/TokenParser.cs
+++ TsysService/TokenParser.cs
@@ -2,11 +2,23 @@
 
 namespace TsysService
 {
+    /// <summary>
+    /// Partial class to support a generated token parser.
+    /// </summary>
     public static partial class TokenParser
     {
+        /// <summary>
+        /// Generated token parser regex definition.
+        /// </summary>
+        /// <returns>A generated token parser regex.</returns>
         [GeneratedRegex(@"^ch_[0-9A-F]{8}-(?:[0-9A-F]{4}-){3}[0-9A-F]{12}$", RegexOptions.IgnoreCase)]
         private static partial Regex TokenRegex();
 
+        /// <summary>
+        /// Parses a token from a string.
+        /// </summary>
+        /// <param name="message">The string from which to parse the token.</param>
+        /// <returns>A token or null if no match was found.</returns>
         public static string? ParseToken(string message)
         {
             var match = TokenRegex().Match(message);
--- TsysService/TsysProcessingValues.cs
+++ TsysService/TsysProcessingValues.cs
@@ -3,6 +3,9 @@ using TsysProcessor.Transaction.Model;
 
 namespace TsysService
 {
+    /// <summary>
+    /// Describes a TSYS variant of the processsing values that uses a TsysTransaction.
+    /// </summary>
     public class TsysProcessingValues : ProcessingValues<TsysTransaction>
     { }
 }
